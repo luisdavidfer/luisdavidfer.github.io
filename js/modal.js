84e3928fc5eb2c2e6dfcf37cd54046bfe513c4af
Luis David <luis.david.fer@gmail.com>
2020-12-21 20:11:08 +0100

    first deploy

--- js/modal.js
+++ js/modal.js
@@ -1,17 +1,18 @@
+// Display contact form
 function displayModal(){
     document.getElementById("container").style.display = "none";
     document.getElementById("modal").style.display = "block";
 }
-
+// Hide contact form
 function closeModal(){
     let inputs = document.getElementsByClassName("form-input");
     for (let i = 0; i < inputs.length; i++) {
         inputs[i].value = "";
     }
     document.getElementById("modal").style.display = "none";
-    document.getElementById("container").style.display = "initial";
+    document.getElementById("container").style.display = "flex";
 }
-
+// Send contact form
 function sendForm(){
     document.getElementsByClassName("form-title")[0].innerHTML = "I'll write you as soon as possible";
     let inputs = document.getElementsByClassName("after-hide");
@@ -23,7 +24,7 @@ function sendForm(){
         document.getElementsByClassName("form-title")[0].innerHTML = "Let's talk !";
         let inputs = document.getElementsByClassName("after-hide");
         for (let i = 0; i < inputs.length; i++) {
-            inputs[i].style.display = "initial";
+            inputs[i].style.display = "flex";
         }
     }, 3000);
 }
